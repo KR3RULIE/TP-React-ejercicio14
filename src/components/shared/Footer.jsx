d1bf5c0cabbde32312bef0f9a88b13d34bc35187
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-07-12 21:45:16 +0200

    maquete el footer

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,8 +1,20 @@
 const Footer = () => {
   return (
-    <div>
-      <h2>Footer</h2>
-    </div>
+    <footer className="text-center fs-5 sombra">
+      <p className="mb-1 mt-3">
+        <span className="text-warning">&copy;</span> Todos los derechos
+        reservaods
+      </p>
+      <p className="mb-2">
+        By{" "}
+        <a
+          href="https://github.com/KR3RULIE"
+          className="text-decoration-none text-info fst-italic"
+        >
+          KR3RULIE
+        </a>
+      </p>
+    </footer>
   );
 };
 
