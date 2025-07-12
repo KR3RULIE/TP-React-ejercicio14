d1b6ffeb438acbca2e14d1184548a3f33196fb52
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-07-12 21:34:06 +0200

    maquete el navbar

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,29 +1,27 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 
 const Header = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand>Simple Recetas</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link>Inicio</Nav.Link>
+            <Nav.Link>¿Sobre nosotros?</Nav.Link>
+          </Nav>
+          <Nav className="ms-auto">
+            <div className="d-flex gap-1 my-auto">
+              <div className="circle"></div>
+              <p className="text-center text-warning my-auto">
+                ADMIN <small className="text-success my-auto">(activo)</small>
+              </p>
+            </div>
+            <Nav.Link>Iniciar Sesión</Nav.Link>
+            <Nav.Link>Cerrar Sesión</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
